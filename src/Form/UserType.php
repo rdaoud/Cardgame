6fb3a4b6991bfd3148a3e9e24c96a2466cc73213
rdaoud <ryan.daoud07@gmail.com>
2020-03-15 23:41:08 +0100

    Inscription

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -23,6 +24,7 @@ class UserType extends AbstractType
                 'first_options'  => array('label' => 'Password'),
                 'second_options' => array('label' => 'Repeat Password'),
             ))
+            ->add('submit', SubmitType::class)
         ;
     }
 
